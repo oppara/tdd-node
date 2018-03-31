661335b57d56e1c5268350b363190078d067a189
oppara <oppara@oppara.tv>
2018-03-31 13:27:05 +0900

    Implement chapter08

--- test/money-test.js
+++ test/money-test.js
@@ -1,27 +1,26 @@
 'use strict';
 
 import assert from 'power-assert';
-import Dollar from '../src/dollar';
-import Franc from '../src/franc';
+import { Money } from '../src/money';
 
 describe('Money', () => {
   it('test multiplication', () => {
-    const five = new Dollar(5);
-    assert.equal(JSON.stringify(new Dollar(10)), JSON.stringify(five.times(2)));
-    assert.equal(JSON.stringify(new Dollar(15)), JSON.stringify(five.times(3)));
+    const five = Money.dollar(5);
+    assert.equal(JSON.stringify(Money.dollar(10)), JSON.stringify(five.times(2)));
+    assert.equal(JSON.stringify(Money.dollar(15)), JSON.stringify(five.times(3)));
   });
 
   it('test equality', () => {
-    assert.strictEqual(new Dollar(5).equals(new Dollar(5)), true);
-    assert.strictEqual(new Dollar(5).equals(new Dollar(6)), false);
-    assert.strictEqual(new Franc(5).equals(new Franc(5)), true);
-    assert.strictEqual(new Franc(5).equals(new Franc(6)), false);
-    assert.strictEqual(new Franc(5).equals(new Dollar(5)), false);
+    assert.strictEqual(Money.dollar(5).equals(Money.dollar(5)), true);
+    assert.strictEqual(Money.dollar(5).equals(Money.dollar(6)), false);
+    assert.strictEqual(Money.franc(5).equals(Money.franc(5)), true);
+    assert.strictEqual(Money.franc(5).equals(Money.franc(6)), false);
+    assert.strictEqual(Money.franc(5).equals(Money.dollar(5)), false);
   });
 
   it('test Franc multiplication', () => {
-    const five = new Franc(5);
-    assert.equal(JSON.stringify(new Franc(10)), JSON.stringify(five.times(2)));
-    assert.equal(JSON.stringify(new Franc(15)), JSON.stringify(five.times(3)));
+    const five = Money.franc(5);
+    assert.equal(JSON.stringify(Money.franc(10)), JSON.stringify(five.times(2)));
+    assert.equal(JSON.stringify(Money.franc(15)), JSON.stringify(five.times(3)));
   });
 });
